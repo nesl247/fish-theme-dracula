dcc9ed87db0a3b01d76ee4f042daadc83002a803
Harrison Heck <harrison.heck@gmail.com>
2016-05-03 12:42:12 -0400

    Changed colors to match dracula.

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -15,48 +15,31 @@ function fish_prompt
     end
   end
 
-  # Setup colors
-  #use extended color pallete if available
-#if [[ $terminfo[colors] -ge 256 ]]; then
-#    turquoise="%F{81}"
-#    orange="%F{166}"
-#    purple="%F{135}"
-#    hotpink="%F{161}"
-#    limegreen="%F{118}"
-#else
-#    turquoise="%F{cyan}"
-#    orange="%F{yellow}"
-#    purple="%F{magenta}"
-#    hotpink="%F{red}"
-#    limegreen="%F{green}"
-#fi
   set -l normal (set_color normal)
   set -l white (set_color FFFFFF)
-  set -l turquoise (set_color 5fdfff)
-  set -l orange (set_color df5f00)
-  set -l hotpink (set_color df005f)
-  set -l blue (set_color blue)
-  set -l limegreen (set_color 87ff00)
-  set -l purple (set_color af5fff)
- 
+  set -l turquoise (set_color 8be9fd)
+  set -l orange (set_color ffb86c)
+  set -l hotpink (set_color ff79c6)
+  set -l blue (set_color 8be9fd)
+  set -l limegreen (set_color 50fa7b)
+  set -l purple (set_color bd93f9)
+
   # Configure __fish_git_prompt
   set -g __fish_git_prompt_char_stateseparator ' '
-  set -g __fish_git_prompt_color 5fdfff
-  set -g __fish_git_prompt_color_flags df5f00
+  set -g __fish_git_prompt_color ff5555
+  set -g __fish_git_prompt_color_flags ffb86c
   set -g __fish_git_prompt_color_prefix white
   set -g __fish_git_prompt_color_suffix white
   set -g __fish_git_prompt_showdirtystate true
   set -g __fish_git_prompt_showuntrackedfiles true
   set -g __fish_git_prompt_showstashstate true
-  set -g __fish_git_prompt_show_informative_status true 
- 
+  set -g __fish_git_prompt_show_informative_status true
+
   # Line 1
-  echo -n $white'╭─'$hotpink$USER$white' at '$orange$__fish_prompt_hostname$white' in '$limegreen(pwd)$turquoise
+  echo -n $white'╭─'$purple$USER$white' at '$hotpink$__fish_prompt_hostname$white' in '$limegreen(pwd)$turquoise
   __fish_git_prompt " (%s)"
   echo
 
   # Line 2
   echo -n $white'╰─'$__fish_prompt_char $normal
 end
-
-
